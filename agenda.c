a86468b3168c4d2737a4fda6897f448d27e08e6c
Cassio <cassio-araujo@outlook.com.br>
2021-03-18 20:40:49 -0300

    Adicionado a função listar

--- agenda.c
+++ agenda.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct
@@ -7,13 +8,71 @@ typedef struct
     char nome[50], telefone[15], email[50], nascimento[10], observacao[10];
 } agenda;
 
-main()
+agenda contato[2];
+
+int listar()
 {
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Codigo: %d\n", contato[i].codigo);
 
-    agenda contato[10];
-    int i, op;
+        printf("Nome: %s\n", contato[i].nome);
 
-    printf("*****************************************************\n");
+        printf("Telefone: %s\n", contato[i].telefone);
+
+        printf("E-mail: %s\n", contato[i].email);
+
+        printf("Data Nascimento: %s\n", contato[i].nascimento);
+
+        printf("Observacoes: %s\n", contato[i].observacao);
+
+        printf("\n");
+    }
+    system("pause");
+}
+
+int cadastrar()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Insira o codigo unico: ");
+        scanf("%d", &contato[i].codigo);
+        while (contato[i].codigo <= 0)
+        {
+            printf("Informe um codigo maior que zero\n");
+            printf("Insira o codigo: ");
+            scanf("%d", &contato[i].codigo);
+        }
+
+        printf("Insira o nome: ");
+        fflush(stdin);
+        gets(contato[i].nome);
+
+        printf("Insira o telefone: ");
+        fflush(stdin);
+        gets(contato[i].telefone);
+
+        printf("Insira o email: ");
+        fflush(stdin);
+        gets(contato[i].email);
+
+        printf("Insira data de nascimento: ");
+        fflush(stdin);
+        gets(contato[i].nascimento);
+
+        printf("Observacoes: ");
+        fflush(stdin);
+        gets(contato[i].observacao);
+
+    }
+}
+
+int menu()
+
+{
+    int op;
+
+    printf("\n*****************************************************\n");
     printf("*                  AGENDA TELEFONICA                *\n");
     printf("*****************************************************\n");
     printf("* 1 - CADASTRAR                                     *\n");
@@ -23,66 +82,28 @@ main()
     printf("* 5 - EXCLUIR                                       *\n");
     printf("* 0 - SAIR                                          *\n");
     printf("*****************************************************\n");
-    printf("Insira uma opcao: ");
+
     scanf("%d", &op);
 
     switch (op)
     {
     case 1:
-        for (int i = 0; i < 10; i++)
-        {
-            printf("Insira o codigo unico: ");
-            scanf("%d", &contato[i].codigo);
-            while(contato[i].codigo <= 0)
-            {
-                printf("Informe um codigo maior que zero\n");
-                printf("Insira o codigo: ");
-                scanf("%d", &contato[i].codigo);
-            }
-
-            printf("Insira o nome: ");
-            fflush(stdin);
-            scanf("%s", contato[i].nome);
-
-            printf("Insira o telefone: ");
-            fflush(stdin);
-            scanf("%s", contato[i].telefone);
-
-            printf("Insira o email: ");
-            fflush(stdin);
-            scanf("%s", contato[i].email);
-
-            printf("Insira data de nascimento: ");
-            fflush(stdin);
-            scanf("%s", contato[i].nascimento);
-
-            printf("Observacoes: ");
-            fflush(stdin);
-            scanf("%s", contato[i].observacao);
-        }
+        cadastrar();
         break;
-
     case 2:
-        printf("Funcao indisponivel");
+        listar();
         break;
-
-    case 3:
-        printf("Funcao indisponivel");
-        break;
-
-    case 4:
-        printf("Funcao indisponivel");
-        break;
-
-    case 5:
-        printf("Funcao indisponivel");
-        break;
-
-    case 0:
-        break;
-
     default:
-        printf("Opcao Invalida");
         break;
+    }
+}
+
+main()
+{
+
+    int op, i;
+    while (op > 0)
+    {
+        menu();
     }
 }
